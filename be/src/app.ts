bfa3bbe0a2a44d91904c5a6b58e030097d86c636
mahou-anisphia <132260755+mahou-anisphia@users.noreply.github.com>
2025-12-27 21:28:46 +0700

    chore: init BE

---
+++ be/src/app.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from "express";
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
